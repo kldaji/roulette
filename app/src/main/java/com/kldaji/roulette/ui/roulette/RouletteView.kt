c39e9ed1c5fe30fad004d227bc28d77952da6544
KLDAJI <kldaji@naver.com>
2022-02-24 12:40:03 +0900

    #3 feat: 룰렛 애니메이션 생성

--- app/src/main/java/com/kldaji/roulette/ui/roulette/RouletteView.kt
+++ app/src/main/java/com/kldaji/roulette/ui/roulette/RouletteView.kt
@@ -6,6 +6,9 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.RotateAnimation
 import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
 import com.kldaji.roulette.R
@@ -28,6 +31,7 @@ class RouletteView @JvmOverloads constructor(
         context.withStyledAttributes(attrs, R.styleable.RouletteView, defStyleAttr, defStyleRes) {
             rouletteViewWidth = getDimension(R.styleable.RouletteView_rouletteViewWidth, 0f)
             rouletteViewHeight = getDimension(R.styleable.RouletteView_rouletteViewHeight, 0f)
+
             initiatePartitionPaint()
         }
         initiatePartitions()
@@ -40,14 +44,12 @@ class RouletteView @JvmOverloads constructor(
     }
 
     private fun initiatePartitions() {
-        with(context.resources) {
-            partitions.add(Partition("빨간색", ContextCompat.getColor(context, R.color.red), 0.2f))
-            partitions.add(Partition("노란색", ContextCompat.getColor(context, R.color.yellow), 0.2f))
-            partitions.add(Partition("초록색", ContextCompat.getColor(context, R.color.green), 0.2f))
-            partitions.add(Partition("파란색", ContextCompat.getColor(context, R.color.blue), 0.2f))
-            partitions.add(Partition("보라색", ContextCompat.getColor(context, R.color.purple), 0.2f))
+        partitions.add(Partition("빨간색", ContextCompat.getColor(context, R.color.red), 0.2f))
+        partitions.add(Partition("노란색", ContextCompat.getColor(context, R.color.yellow), 0.2f))
+        partitions.add(Partition("초록색", ContextCompat.getColor(context, R.color.green), 0.2f))
+        partitions.add(Partition("파란색", ContextCompat.getColor(context, R.color.blue), 0.2f))
+        partitions.add(Partition("보라색", ContextCompat.getColor(context, R.color.purple), 0.2f))
 
-        }
         var startAngle = 270f
         partitions.forEach { partition ->
             val sweepAngle = 360 * partition.ratio
@@ -90,5 +92,24 @@ class RouletteView @JvmOverloads constructor(
 
     private fun setPartitionPaintColor(_color: Int) {
         partitionPaint.color = _color
+    }
+
+    fun startRouletteViewAnimation(rotateDegree: Float, duration: Long) {
+        val rotateAnimation = createRotateAnimation(rotateDegree, duration)
+        startAnimation(rotateAnimation)
+    }
+
+    private fun createRotateAnimation(rotateDegree: Float, duration: Long): RotateAnimation {
+        return RotateAnimation(0f,
+            rotateDegree,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f
+        ).apply {
+            this.duration = duration
+            fillAfter = true
+            interpolator = DecelerateInterpolator(1.5f)
+        }
     }
 }
