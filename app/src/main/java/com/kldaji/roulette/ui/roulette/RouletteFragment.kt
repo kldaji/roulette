fdfa053958c005240f7bd8b54dd51eb474c02f7c
KLDAJI <kldaji@naver.com>
2022-02-24 12:41:10 +0900

    #3 feat: 룰렛 애니메이션 시작 버튼 터치 리스너 등록 및 애니메이션 실행

--- app/src/main/java/com/kldaji/roulette/ui/roulette/RouletteFragment.kt
+++ app/src/main/java/com/kldaji/roulette/ui/roulette/RouletteFragment.kt
@@ -1,11 +1,14 @@
 package com.kldaji.roulette.ui.roulette
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.kldaji.roulette.R
 import com.kldaji.roulette.databinding.FragmentRouletteBinding
@@ -27,6 +30,7 @@ class RouletteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         addRoulettePinView()
+        startButtonTouchListener()
     }
 
     private fun addRoulettePinView() {
@@ -37,6 +41,25 @@ class RouletteFragment : Fragment() {
             pinView.roulettePinBitmap = it
         }
         binding.root.addView(pinView)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun startButtonTouchListener() {
+        binding.startBtnRoulette.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.performClick()
+                }
+                MotionEvent.ACTION_UP -> {
+                    v.performClick()
+                    val rotateDegree = (3000..10000).random().toFloat()
+                    val duration = (rotateDegree * 2).toLong()
+                    binding.rouletteView.startRouletteViewAnimation(rotateDegree, duration)
+                    binding.startBtnRoulette.isVisible = false
+                }
+            }
+            false
+        }
     }
 
     override fun onDestroyView() {
